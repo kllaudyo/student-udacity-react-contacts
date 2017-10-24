1e15795eb54b1086a3f70bcf28e25ce2acd8c4c5
Claudio Acioli <kllaudyo@gmail.com>
2017-10-24 20:09:11 -0200

    Minha solução para chamar api, não usei o import * from, nem atribui de forma muito elegante o estado

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import {getAll} from './utils/ContactsAPI';
 
 class App extends Component {
 
     state = {
-        contacts : [
-            {
-                "id": "ryan",
-                "name": "Ryan Florence",
-                "email": "ryan@reacttraining.com",
-                "avatarURL": "http://localhost:5001/ryan.jpg"
-            },
-            {
-                "id": "michael",
-                "name": "Michael Jackson",
-                "email": "michael@reacttraining.com",
-                "avatarURL": "http://localhost:5001/michael.jpg"
-            },
-            {
-                "id": "tyler",
-                "name": "Tyler McGinnis",
-                "email": "tyler@reacttraining.com",
-                "avatarURL": "http://localhost:5001/tyler.jpg"
-            }
-        ]
+        contacts : []
     };
 
     removeContact = (contact) => {
@@ -34,13 +16,9 @@ class App extends Component {
         }))
     };
 
-    // removeContact = function(contact){
-    //     this.setState(function(state){
-    //         state.contacts = state.contacts.filter(function(c){
-    //             return c.id !== contact.id;
-    //         })
-    //     })
-    // };
+    componentDidMount(){
+        getAll().then(data => this.setState(this.state.contacts = data))
+    }
 
     render() {
         return (
